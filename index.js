52e4d638edca9c2aac874fc6c01c4b8e175c9c7b
identityapproved <identityapproved@gmail.com>
2024-01-18 15:14:44 +0200

    Basic authorization with passport.js, jwt, bcript

--- index.js
+++ index.js
@@ -1,18 +1,47 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const path = require('path');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
+const config = require("./config/db.js");
+const account = require("./routes/account.js");
 
 const app = express();
 
 const port = 3000;
 
-app.listen(port, ()=>{
-  console.log("Server running on port: " + port)
+app.use(require('express-session')({
+  secret: config.secret,
+  resave: true,
+  saveUninitialized: true
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport.js')(passport);
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+mongoose.connect(config.db);
+
+mongoose.connection.on('connected', () => {
+  console.log('Successful connected to the database')
 });
 
-app.get('/', (req, res) => {
-  res.send("Main page!")
+mongoose.connection.on('error', (err) => {
+  console.log('Error while connecting to database: ' + err)
 })
+
+app.listen(port, () => {
+  console.log("Server running on port: " + port);
+});
+
+app.get("/", (req, res) => {
+  res.send("Main page!");
+});
+
+app.use('/account', account)
